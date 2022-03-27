3946a4f1ed75d8d713b9f395299c028b2b3673f3
Tinastouch <tinastouch@gmail.com>
2022-03-26 20:47:12 -0400

    first commit

---
+++ models/Thought.js
@@ -0,0 +1,33 @@
+const dateFormat = require('../utils/dateFormat');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText:{
+            type: String,
+            required: 'You need to leave a thought!',
+            minlength: 1,
+            maxlength: 280
+        },
+
+    createdAt:{
+        type: Date,
+        default: Date.now,
+        get: timestamp => dateFormat(timestamp)
+    },
+    username:{
+        type: String,
+        required: true
+    },
+    reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            getters: true
+        },
+        id: false
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function(){
+    
+})
